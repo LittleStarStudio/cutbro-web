7d0c613b566cf1f52414f6f43daece09bcfbb5fb
athalaraharsha-cloud <athalaraharsha@gmail.com>
2026-02-02 10:54:12 +0700

    feat: Init react ts + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">CutBro</h1>
+    </div>
+  )
+}
